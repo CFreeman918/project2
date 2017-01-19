9a8ab2e375434bdf7116b800a2a0902d54e294a2
cfreeman918 <colbyfreeman918@gmail.com>
2017-01-19 10:20:48 -0600

    Clean up
    Added todo list. Fixed nav for BS4.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,28 +1,24 @@
-
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#"><i class="fa fa-home" aria-hidden="true"></i>Daddy's Home Inventory</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">View</a></li>
-            <li><a href="#about">Add</a></li>
-            <li><a href="#contact">Remove</a></li>
-          </ul>
-          @if (Route::has('login'))
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="{{ url('/login') }}">Login</a></li>
-            <li><a href="{{ url('/register') }}">Register</a></li>
-          </ul>
-          @endif  
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+<!-- Fixed navbar -->
+<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">My Home Inventory</a>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Add Item</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Modify Item</a>
+      </li>
+    </ul>
+    <form class="form-inline mt-2 mt-md-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,17 @@
 @extends('layouts.master')
 @section('title')
-Welcome
+Home Inventory - Welcome!
 @stop
-@section('content')
-
-            
-                <div class="container">
-            
-                  <form class="form-signin">
-                    <h2 class="form-signin-heading">Please sign in</h2>
-                    <label for="inputEmail" class="sr-only">Email address</label>
-                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-                    <label for="inputPassword" class="sr-only">Password</label>
-                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
-                    <div class="checkbox">
-                      <label>
-                        <input type="checkbox" value="remember-me"> Remember me
-                      </label>
-                    </div>
-                    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                  </form>
-            
-                </div> <!-- /container -->
-            
-            
-                
-            
-            
-        
+@section('content')            
+<div class="container-fluid">
+  <div class="panel panel-default">
+    <div class="panel-heading">ToDo</div>
+    <div class="panel-body">
+      <ul>
+        <li>Make form to input items and figure out what fields are required. </li>
+        <li>Customize nav to remove default links.</li>
+      </ul>
+    </div>
+  </div>
+</div> <!-- /container -->
 @stop
